cce6ef6465fe24c0a4d3325022fc169f1dd6f8fc
xxadi-07 <adityanaskar2021@gmail.com>
2025-10-23 02:04:24 +0530

    setup

---
+++ flowgen/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button>
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
