285ab6a8537e6a26a933a6ee7aed2db7adab1aaa
xxadi-07 <adityanaskar2021@gmail.com>
2025-10-30 02:09:16 +0530

    04:authetication

--- flowgen/src/trpc/server.tsx
+++ flowgen/src/trpc/server.tsx
@@ -1,9 +1,9 @@
 import 'server-only'; // <-- ensure this file cannot be imported from the client
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import { cache } from 'react';
-import { createTRPCContext } from '../init';
+import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
-import { appRouter } from './_app';
+import { appRouter } from './routers/_app';
 import { QueryClient } from '@tanstack/react-query';
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
